00b2d9265395c205f4d0df57ca3b7092d5046fe3
sovonthy <sovonthy.leng069@gmail.com>
2021-11-01 11:51:41 +0700

    - add navigation drawer.

--- app/src/main/java/com/test/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/test/bottomnavigation/MainActivity.kt
@@ -3,14 +3,23 @@ package com.test.bottomnavigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initViewAction()
+        intNavigationDrawer()
     }
 
     private fun initViewAction(){
@@ -28,6 +37,43 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+            true
+        }
+    }
+
+    private fun intNavigationDrawer(){
+
+        val actionbar = supportActionBar
+        actionbar!!.title = "My Activity"
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarToggle)
+        actionBarToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView = findViewById(R.id.navView)
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.setting -> {
+                    Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+            // Close the drawer
+            drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
